1ab1dcf61b71d82215f2dfab293f19909d4fcddd
prostreet1212@gmail.com <ktndlf,tcfyu2020>
2023-12-17 14:29:05 +0300

    camera page

--- lib/presentation/pages/camera_page.dart
+++ lib/presentation/pages/camera_page.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
+import 'package:custom_image_picker/custom_image_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class CameraPage extends StatefulWidget {
   const CameraPage({Key? key}) : super(key: key);
@@ -9,35 +13,115 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
-
   late List<CameraDescription> cameras;
   late CameraController _cameraController;
-late List<dynamic> _galleryPhotos;
+  List<String> _galleryPhotos = [];
+
   @override
   void initState() {
+    initializeCamera();
+    getImagesFromGallery();
     super.initState();
-
   }
 
-  Future<void>initializeCamera()async{
-    cameras=await availableCameras();
-    _cameraController=CameraController(cameras[0], ResolutionPreset.medium);
+  Future<void> initializeCamera() async {
+    cameras = await availableCameras();
+    _cameraController = CameraController(cameras[0], ResolutionPreset.medium);
     _cameraController.initialize().then((value) {
-      if(mounted)return;
+      if (mounted) return;
       setState(() {});
     });
   }
+  Future<void> getImagesFromGallery() async {
 
-  Future<void> getImagesFromGallery()async{
+    await CustomImagePicker().getAllImages(callback: (value) {
+      setState(() {
+        _galleryPhotos = value;
+      });
+      _galleryPhotos.forEach((element) {
+        print(element);
+      });
+    }).then((value) {
+    print('a');
+    });
 
   }
 
   @override
   Widget build(BuildContext context) {
+    if (!_cameraController.value.isInitialized) {
+      return Container();
+    }
     return Scaffold(
-      body: Center(
-        child: Text('Camera Page'),
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: CameraPreview(_cameraController),
+          ),
+          _galleryWidget(),
+          _cameraButtonWidget(),
+        ],
       ),
     );
+  }
+
+  Widget _cameraButtonWidget() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.flash_on,
+              color: Colors.white,
+              size: 30,
+            ),
+            Container(
+              height: 80,
+              width: 80,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(50)),
+                border: Border.all(color: Colors.white, width: 2),
+              ),
+            ),
+            Icon(
+              Icons.camera_alt,
+              size: 30,
+              color: Colors.white,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _galleryWidget() {
+    return Positioned(
+        bottom: 100,
+        right: 0,
+        left: 0,
+        child: Container(
+          height: 55,
+          color: Colors.orange,
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: _galleryPhotos.length,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.only(right: 8),
+                  height: 55,
+                  width: 50,
+                  decoration: BoxDecoration(color: Colors.red.withOpacity(.2)),
+                  child: Image.file(
+                    File(_galleryPhotos[index]),
+                    fit: BoxFit.cover,
+                  ),
+                );
+              }),
+        ));
   }
 }
--- lib/presentation/pages/chat_page.dart
+++ lib/presentation/pages/chat_page.dart
@@ -1,6 +1,9 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:whatsapp_clone/presentation/pages/sub_pages/singe_item_chat_user_page.dart';
 import 'package:whatsapp_clone/presentation/widgets/theme/style.dart';
+
 
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -21,7 +24,9 @@ class ChatPage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         backgroundColor: primaryColor,
         child: Icon(Icons.chat),
-        onPressed: () {  },),
+        onPressed: () async{
+
+        },),
     );
   }
 }
